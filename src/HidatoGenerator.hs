3791d0aadf6214c20dff3d56ab2cf25a3e093aac
Leismael <leismael.sosa17@gmail.com>
2022-07-11 06:29:11 -0400

    Base for generating unique hidatos

--- src/HidatoGenerator.hs
+++ src/HidatoGenerator.hs
@@ -3,7 +3,31 @@ module HidatoGenerator where
 
 -- {-# LANGUAGE BlockArguments #-}
 import HidatoCommon
+import qualified Utils
 
 squareBoard :: Int -> Int -> HidatoBoard
 squareBoard n m =  
     Board {board = [[0 | _ <- [1..n]] | _ <- [1..m]], minVal = 1, maxVal = n*m}
+
+
+findHidatosSolutions :: HidatoBoard -> (Int, Int) -> [Maybe HidatoBoard]
+findHidatosSolutions hidatoBoard (x,y)
+    | not (validBoard hidatoBoard) = [Nothing]
+    | gameEnd hidatoBoard = [Just hidatoBoard]
+    | otherwise = do
+        let dirs = directionFilter (board hidatoBoard) (x,y) directions -- (6,7) --> (7,6)
+        let nextNumber = (board hidatoBoard !! x !! y) + 1 -- 5
+        if dirs == [] then [Nothing]
+        else do
+            solveHidatos' hidatoBoard dirs nextNumber
+
+solveHidatos' :: HidatoBoard -> [(Int, Int)] -> Int -> [Maybe HidatoBoard]
+-- TODO: Resolver esto
+-- solveHidato' hidatoBoard [] num = Just hidatoBoard
+solveHidatos' _ [] _ = [Nothing]
+solveHidatos' hidatoBoard (x:xs) num = do
+    let hidatoBoard' = Utils.replaceAt x num $ board hidatoBoard
+    let res = findHidatosSolutions (Board hidatoBoard' (minVal hidatoBoard) (maxVal hidatoBoard)) x
+    -- let resHidato = extractHidatoBoard res
+    let otherSolutions = solveHidatos' hidatoBoard xs num
+    res <> otherSolutions

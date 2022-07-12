8959639cdf7f4a5f2033bcdd2ec8339a1e4bee99
Leismael <leismael.sosa17@gmail.com>
2022-07-12 15:04:13 -0400

    Removed excesive comments

--- app/MainGenerator.hs
+++ app/MainGenerator.hs
@@ -63,41 +63,24 @@ main = do
     -- let hidato = squareBoard 10 10
     let hidatoBoard = convertToHidatoBoard table
     let initialPos = findElement (board hidatoBoard) (minVal hidatoBoard)
-    -- let uniqueSolutions = findSolution hidatoBoard initialPos
+
     currTime <- getCurrentTime
     let timed = floor $ utctDayTime currTime :: Int
     let seed = mkStdGen (timed)
-    -- let uniqueHidato = generateUniqueHidato hidatoBoard ran
+
     let uniqueHidato = generateUniqueHidato hidatoBoard seed
-    if isNothing uniqueHidato then putStrLn "No hay un tablero único??... Mhmm, Extraño\n\n"
+    if isNothing uniqueHidato then putStrLn "No se puede generar un tablero único... El tablero inicial esta invalido\n\n"
     else do
       putStrLn "Hidato con solución única:\n\n"
       putStrLn $ showBoard $ fromJust uniqueHidato
       putStrLn "\n\n--------------------------------\n\n"
-      -- let allSolutions = findHidatosSolutions uniqueHidato initialPos
     
     let uniqueSolutions = findHidatoSolution (fromJust uniqueHidato) initialPos seed
     if isNothing uniqueSolutions then putStrLn "El Hidato no tiene solución"
     else do
-      let (cells, solution, moreThanOne) = fromJust uniqueSolutions
+      let (_, solution, moreThanOne) = fromJust uniqueSolutions
       putStrLn "Una solución es:\n\n"
       putStrLn $ showBoard solution
-      putStrLn "\n\nLas posiciones agregadas fueron:\n\n"
-      print cells
       if moreThanOne then putStrLn "Hay mas de una solución\n"
       else do
         putStrLn "\n\n----------------------------------------"
-
-        -- let newHidato = addCellsToHidatoBoard hidatoBoard cells
-        -- putStrLn $ "El hidato con solución única es: \n\n" <> (showBoard newHidato)
-        -- putStrLn $ "La solución única de este Hidato es:\n\n" <> (showBoard solution)
-
-    -- let solutions = findHidatosSolutions hidatoBoard initialPos
-    -- let sol = filter isJust solutions
-    -- printHidatosSolutions sol
-    -- -- let possibleNumber = getLine
-    -- -- putStrLn $ possibleNumber >>= printNumber
-    -- -- getLine >>= (\line -> putStrLn $ printNumber line)
-    -- -- fromMaybe 
-    -- -- let parsedNumber = readMaybe possibleNumber :: Maybe Int
-    -- putStrLn (showBoard hidato)
